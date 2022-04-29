6eb2afdfbb408a9bab81049015b92078cf270074
joaogcarlosf <joaogcarlosf@gmail.com>
2022-04-29 10:39:50 -0300

    creating routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,10 @@
 import express from "express" // para instalar a tipagem precisa do npm i @types/express
+import routes from "./routes"
 
 
 const app = express()
 
-app.get('/', (req, res) => {
-    return res.send('Hello World')
-})
-
-
+app.use(routes)
 
 
 app.listen(5500) // criação do server backend na porta 5500
