9d47433d8a765a18bda4febae6a463ca82b125aa
joaogcarlosf <joaogcarlosf@gmail.com>
2022-04-29 10:22:35 -0300

    api to hello world

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,4 +3,11 @@ import express from "express" // para instalar a tipagem precisa do npm i @types
 
 const app = express()
 
+app.get('/', (req, res) => {
+    return res.send('Hello World')
+})
+
+
+
+
 app.listen(5500) // criação do server backend na porta 5500
