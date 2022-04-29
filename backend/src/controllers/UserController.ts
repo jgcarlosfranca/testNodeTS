7ca6e2698388b9a5314347cb8025a385b20e56be
joaogcarlosf <joaogcarlosf@gmail.com>
2022-04-29 10:45:24 -0300

    fixing definition

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from "express" // tipagem do req e do res
 
-const user = [
+const users = [
     { name: 'Abc', email: 'abc@gmail.com', serie: 123 }
 ]
 
 export default {
     async index(req: Request, res: Response){ // tipagem do req e do res
-        return res.json(user)
+        return res.json(users)
     }
 }
