84bccf07852576b41fe03ba5b7019d24b3f522fc
joaogcarlosf <joaogcarlosf@gmail.com>
2022-05-02 22:15:27 -0300

    call userController and email Service

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,11 +1,22 @@
 import { Request, Response } from "express" // tipagem do req e do res
+import EmailService from "../services/EmailService"
 
 const users = [
     { name: 'Abc', email: 'abc@gmail.com', serie: 123 }
 ]
 
 export default {
-    async index(req: Request, res: Response){ // tipagem do req e do res
+    async index(req: Request, res: Response) { // tipagem do req e do res
         return res.json(users)
+    },
+
+    async create(req: Request, res: Response) {
+        const emailService = new EmailService()
+
+        emailService.sendMail(
+            { name: 'teste', email: 'teste@gmail' },
+            { subject: 'email title', body: 'testing my api to send email' }
+        )
+
     }
 }
