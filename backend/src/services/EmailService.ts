40ea973e251a0d1a4d78aa172feccb13f5a36d5e
joaogcarlosf <joaogcarlosf@gmail.com>
2022-05-03 21:50:28 -0300

    implements

--- backend/src/services/EmailService.ts
+++ backend/src/services/EmailService.ts
@@ -11,15 +11,19 @@ interface IMailMsg {
 
 }
 
-interface MessageDTO {
+interface IMessageDTO {
     to: IMailTo
     message: IMailMsg
 }
 
 //-DTO data transfer object (métodologia DDD)
 
-class EmailService {
-    sendMail({ to, message }: MessageDTO) {
+interface IEmailService{
+    sendMail(resquest: IMessageDTO): void
+}
+
+class EmailService implements IEmailService{
+    sendMail({ to, message }: IMessageDTO) {
         console.log(`Email enviado para ${to.name}: ${message.subject}`)
     }
 
